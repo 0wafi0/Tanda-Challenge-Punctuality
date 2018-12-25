c4ffbbfc03d1a0d2bb23bbb23c1783042a41fb9f
GOD\Wafi <wafi@outlook.com>
2018-12-25 18:56:55 +1000

    Initial commit and work. Added calendar and header

---
+++ scripts.js
@@ -0,0 +1,18 @@
+$('input[name="daterange"]').daterangepicker({
+	ranges: {
+		'This Pay Period': [moment().subtract(moment().day(), 'days'), moment()],
+		'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+		'This Month': [moment().startOf('month'), moment().endOf('month')],
+		'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+	},
+	"alwaysShowCalendars": true,
+	"startDate": moment().subtract(moment().day(), 'days'),
+	"endDate": moment(),
+	"maxDate": moment(),
+	"showWeekNumbers": true,
+	"opens": "left"
+}, function(start, end, label) {
+  console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + "asdasdas" + ')');
+});
+
+console.log($('input[name="daterange"]').data('daterangepicker').startDate.format('YYYY-MM-DD'));
